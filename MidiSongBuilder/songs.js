66dca4bfeb88cf64a2f11a9440a5a78eb7818337
austin183 <austin183@gmail.com>
2024-08-16 21:28:00 -0500

    Fixing missing note in the KeyNoteMaps.  Added National Anthem and a way to see which notes and keys go with it.

---
+++ MidiSongBuilder/songs.js
@@ -0,0 +1,324 @@
+function getSongs(){
+    return {
+        "NationalAnthem" : [
+            //First Row
+            {
+                "note": "B3"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "E3"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "E4"
+            },
+            //Second Row
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "A#3"
+            },
+            {
+                "note": "B3"
+            },
+            //Third Row
+            {
+                "note": "B3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "D#4"
+            },
+            //Fourth Row
+            {
+                "note": "C#4"
+            },
+            {
+                "note": "D#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "E3"
+            },
+            //Second Verse Fisrt Row
+            {
+                "note": "B3"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "E3"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "E4"
+            },
+            //Second Verse Second Row
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "A#3"
+            },
+            {
+                "note": "B3"
+            },
+            //Second Verse Third Row
+            {
+                "note": "B3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "D#4"
+            },
+            //Second Verse Fourth Row
+            {
+                "note": "C#4"
+            },
+            {
+                "note": "D#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "E3"
+            },
+            //Third Verse First Row
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "B4"
+            },
+            {
+                "note": "B4"
+            },
+            //Third Verse Second Row
+            {
+                "note": "A4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "A4"
+            },
+            //Fourth Verse Third Row
+            {
+                "note": "A4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "D#4"
+            },
+            //Fourth Verse Fourth Row
+            {
+                "note": "C#4"
+            },
+            {
+                "note": "D#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G#3"
+            },
+            {
+                "note": "A#3"
+            },
+            {
+                "note": "B3"
+            },
+            //Fifth Verse First Row
+            {
+                "note": "B3"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "D#4"
+            },
+            {
+                "note": "C#4"
+            },
+            {
+                "note": "C#4"
+            },
+            {
+                "note": "C#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "D#4"
+            },
+            //Fifth Verse Second Row
+            {
+                "note": "B3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "A#4"
+            },
+            {
+                "note": "B4"
+            },
+            //Fifth Verse Third Row
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "G#4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "E4"
+            }
+        ]
+    }
+}
