d310d89af2d2273b9cc0323cf706de2aaa09ea8c
austin183 <austin183@gmail.com>
2024-08-17 21:33:21 -0500

    Adding Mario Bros theme song

--- MidiSongBuilder/songs.js
+++ MidiSongBuilder/songs.js
@@ -430,6 +430,431 @@ function getSongs(){
             {
                 "note": "C4"
             },
+        ],
+        "MarioBrosTheme":[
+            //First section
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "G3"
+            },
+            //Second section
+            {
+                "note": "C4"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "E3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "A#3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "B3"
+            },
+            //Third Section
+            {
+                "note": "G4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C5"
+            },
+            {
+                "note": "C5"
+            },
+            {
+                "note": "C5"
+            },
+            //Next
+            {
+                "note": "G4"
+            },
+            {
+                "note": "F#4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "D#4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "C4"
+            },
+            //Next
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "E4"
+            },
+            //Next
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "G3"
+            },
+            //Next
+            {
+                "note": "C4"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "E3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "A#3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "B3"
+            },
+            //Next
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "A4"
+            },
+            {
+                "note": "G4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "G3"
+            },
+            //Next
+            {
+                "note": "E4"
+            },
+            {
+                "note": "C4"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "G3"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "A3"
+            },
+            {
+                "note": "B3"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "F4"
+            },
+            {
+                "note": "E4"
+            },
+            {
+                "note": "D4"
+            },
+            {
+                "note": "C4"
+            }
         ]
     }
 }
