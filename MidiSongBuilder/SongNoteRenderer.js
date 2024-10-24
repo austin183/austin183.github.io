8197ac319d8743d2e81f6776b0f5c463024682c0
austin183 <austin183@gmail.com>
2024-10-24 13:00:06 -0500

    Separating out the visible field filter code so I could use it when scoring the payer later.  Hiding the text renderer and saving the processor cycles if not in debug mode.

--- MidiSongBuilder/SongNoteRenderer.js
+++ MidiSongBuilder/SongNoteRenderer.js
@@ -127,54 +127,7 @@ function getSongNoteRenderer(){
             }
             return notesToPlay;
         },
-        renderNotesPlayingForCanvas: function(canvas, ctx, song, invertedKeyNoteMap, keyRenderInfo, now, keyCount){
-            //Set the visible field to the next 10 seconds of the song
-            var futureLimit = now + 10;
-            var bufferFutureLimit = now + 20;
-            var pastLimit = now - 5;
-            var visibleField = [];            
-            for(var i = 0; i < song.length; i++){
-                var note = song[i];
-                var noteEnd = note.time + note.duration;
-                //Fast Forward to the section of song playing
-                if(pastLimit > noteEnd){
-                    continue;                    
-                }
-                //Build dataset for visible field
-                else if(bufferFutureLimit >= noteEnd){
-                    var keyNote = invertedKeyNoteMap[note.name];
-                    if(!keyNote){
-                        continue;
-                    }
-
-                    var canvasNote = {
-                        duration : note.duration,
-                        time : note.time,
-                        letter : keyNote,
-                        id: note.name + "_" + note.time
-                    };
-                    visibleField.push(canvasNote);
-                }
-                else{
-                    break;
-                }
-            }
-
-            //Remove overlaps greater than the keyCount
-            for(var j = visibleField.length - 1; j >= 0; j--){
-                var canvasNote = visibleField[j];
-                var overlap = visibleField.filter(n => n.id !== canvasNote.id && (n.time < canvasNote.time + canvasNote.duration && n.time + n.duration > canvasNote.time));
-                if(overlap.length >= keyCount){
-                    //Remove overlapping note                
-                    visibleField.splice(visibleField.indexOf(canvasNote), 1);
-                }
-            }
-
-            //Remove notes that ended before now
-            visibleField = visibleField.filter(function(note) {
-              return note.time + note.duration >= now && note.time + note.duration <= futureLimit;
-            });                
-            
+        renderNotesPlayingForCanvas: function(canvas, ctx, visibleField, keyRenderInfo, now){
             //Render the visibleField to the canvas
             visibleField.forEach(canvasNote => {                
                 //Create new note and calculate position for it
@@ -183,5 +136,4 @@ function getSongNoteRenderer(){
             });
         }
     };
-    
 }
---
+++ MidiSongBuilder/VisibleFieldFilterer.js
@@ -0,0 +1,51 @@
+function getVisibleFieldFilterer(){
+    return {
+        filterToVisibleField: function(song, pastLimit, bufferFutureLimit, futureLimit, now, invertedKeyNoteMap, keyCount) {
+            var visibleField = [];
+            for (var i = 0; i < song.length; i++) {
+                var note = song[i];
+                var noteEnd = note.time + note.duration;
+                //Fast Forward to the section of song playing
+                if (pastLimit > noteEnd) {
+                    continue;
+                }
+
+                //Build dataset for visible field
+                else if (bufferFutureLimit >= noteEnd) {
+                    var keyNote = invertedKeyNoteMap[note.name];
+                    if (!keyNote) {
+                        continue;
+                    }
+
+                    var canvasNote = {
+                        duration: note.duration,
+                        time: note.time,
+                        letter: keyNote,
+                        id: note.name + "_" + note.time
+                    };
+                    visibleField.push(canvasNote);
+                }
+                else {
+                    break;
+                }
+            }
+
+            //Remove overlaps greater than the keyCount
+            for (var j = visibleField.length - 1; j >= 0; j--) {
+                var canvasNote = visibleField[j];
+                var overlap = visibleField.filter(n => n.id !== canvasNote.id && (n.time < canvasNote.time + canvasNote.duration && n.time + n.duration > canvasNote.time));
+                if (overlap.length >= keyCount) {
+                    //Remove overlapping note                
+                    visibleField.splice(visibleField.indexOf(canvasNote), 1);
+                }
+            }
+
+            //Remove notes that ended before now
+            visibleField = visibleField.filter(function(note) {
+                return note.time + note.duration >= now && note.time + note.duration <= futureLimit;
+            });  
+
+            return visibleField;
+        }
+    };
+}
