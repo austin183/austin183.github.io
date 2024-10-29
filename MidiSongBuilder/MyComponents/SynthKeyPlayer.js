e1f7d8911956beffd678b4b3f820c2b12f463184
austin183 <austin183@gmail.com>
2024-10-28 21:11:12 -0500

    Introducing a slight delay to the key player to help with crackling and popping when the audio scheduler gets overwhelmed.

--- MidiSongBuilder/MyComponents/SynthKeyPlayer.js
+++ MidiSongBuilder/MyComponents/SynthKeyPlayer.js
@@ -30,7 +30,7 @@ function getSynthKeyPlayer(){
                     }
                     //Start playing the note for the key through the synth
                     //based on the pressed key's not in the selected keyNoteMap
-                    synthArray[synthIndex].triggerAttack(app.selectedKeyNoteMap.keyNoteMap[key], "0", velocity);
+                    synthArray[synthIndex].triggerAttack(app.selectedKeyNoteMap.keyNoteMap[key], ".01", velocity);
                     //Save which synth goes with the key right now
                     synthMap[key] = synthIndex;
                 } else {
