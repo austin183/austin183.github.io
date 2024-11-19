422da0b5de1578b8f3fe216e20e790afb711b376
austin183 <austin183@gmail.com>
2024-11-18 19:35:54 -0600

    Adding experimental feature to load midi files directly.  This could shrink file sizes and save time converting them to json files.

---
+++ MidiSongBuilder/MyComponents/midiSongList.js
@@ -0,0 +1,13 @@
+function getMidiSongList() {
+    return {
+        "For All the Seasons": {
+            filename: "anon_als_com.mid"
+        },
+        "The First Noel": {
+            filename: "anon_firnoel_com.mid"
+        },
+        "Jingle Bells": {
+            filename: "anon_jbells_com.mid"
+        }
+    };
+}
