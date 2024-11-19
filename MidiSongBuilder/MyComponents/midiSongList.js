03cfe79344092bc4e53eef8fd2f8efb2110d40bb
austin183 <austin183@gmail.com>
2024-11-18 21:00:19 -0600

    Adding more songs to the midi drop down.

--- MidiSongBuilder/MyComponents/midiSongList.js
+++ MidiSongBuilder/MyComponents/midiSongList.js
@@ -8,6 +8,33 @@ function getMidiSongList() {
         },
         "Jingle Bells": {
             filename: "anon_jbells_com.mid"
+        },
+        "Thoinot Arbeau - Les Bouffons (Mattachins)": {
+            filename: "arbeau_matachins.mid"
+        },
+        "Johann Sebastian Bach - 2 Part Inventions": {
+            filename: "bach_2partInvention.mid"
+        },
+        "Johann Sebastian Bach - Brandenburg Concerto #2 in F": {
+            filename: "bach_branconc2F_I_com.mid"
+        },
+        "Johann Sebastian Bach - Brandenburg Concerto #2 in F (III)": {
+            filename: "bach_branconc2F_III_com.mid"
+        },
+        "Johann Sebastian Bach - Invention 1 c": {
+            filename: "bach_inv1C_COM.MID"
+        },
+        "Johann Sebastian Bach - Gavotte": {
+            filename: "bach_gavotte.mid"
+        },
+        "Johann Sebastian Bach - Prelude 1": {
+            filename: "bach_prelude1.mid"
+        },
+        "Johann Sebastian Bach - The Gigue Fugue": {
+            filename: "bach_theGigueFugue.mid"
+        },
+        "Beethoven - Symphony 6": {
+            filename: "beetho_sym6F_I_com.mid"
         }
     };
 }
