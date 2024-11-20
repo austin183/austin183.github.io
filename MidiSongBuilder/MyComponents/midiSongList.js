63b77d9f52ad475f6412b3d46c49eaf5cebe65cd
Austin <austin183@gmail.com>
2024-11-19 19:36:55 -0600

    Adding remaining Public Domain songs

--- MidiSongBuilder/MyComponents/midiSongList.js
+++ MidiSongBuilder/MyComponents/midiSongList.js
@@ -41,6 +41,63 @@ function getMidiSongList() {
         },
         "Debussy - Golliwog's Cakewalk": {
             filename: "debussy_gollycake.mid"
+        },
+        "Edvard Grieg - Hall of the Mountain King from Peer Gynt Suite No. 1": {
+            filename: "grieg_hallofking.mid"
+        },
+        "George Frideric Handel - Music for the Royal Fireworks, HWV 351": {
+            filename: "handel_firewk_mus.mid"
+        },
+        "George Frideric Handel - Sheba from Judas Maccabeus, HWV 63 (piano arrangement)": {
+            filename: "handel_sheba_pno.mid"
+        },
+        "Wolfgang Amadeus Mozart - Piano Concerto No. 21 in C major, K. 467 (II. Andante) - Elvira Madigan": {
+            filename: "mozart-piano-concerto-21-2-elvira-madigan-piano-solo.mid"
+        },
+        "Wolfgang Amadeus Mozart - Elvira Madigan from Piano Concerto No. 21 in C major, K. 467": {
+            filename: "mozart_elvira_m.mid"
+        },
+        "Erik Satie - Gymnopedie No. 1 for Piano Solo": {
+            filename: "satie_gymno1.mid"
+        },
+        "Erik Satie - Gymnopedie No. 2 for Piano Solo": {
+            filename: "satie_gymno2.mid"
+        },
+        "Erik Satie - Gymnopedie No. 3 for Piano Solo": {
+            filename: "satie_gymno3.mid"
+        },
+        "Franz Schubert - Serenade from Schwanengesang, D. 957": {
+            filename: "schubert_serenade.mid"
+        },
+        "Franz Schubert - Symphony No. 5 in B-flat major, D. 485 (I. Allegro)": {
+            filename: "schubert_symph_5.mid"
+        },
+        "Robert Schumann - Soldier's March from Album für die Jugend, Op. 68": {
+            filename: "schumann_soldier_m.mid"
+        },
+        "Johann Strauss II - The Blue Danube Waltz, Op. 314 (II. Trio)": {
+            filename: "straus_blued_II_com.mid"
+        },
+        "Johann Strauss II - The Blue Danube Waltz, Op. 314 (III. Refrain)": {
+            filename: "straus_blued_III_com.mid"
+        },
+        "Johann Strauss II - The Blue Danube Waltz, Op. 314 (IV. Finale)": {
+            filename: "straus_blued_IV_com.mid"
+        },
+        "Pyotr Ilyich Tchaikovsky - Swan Lake from Ballet Suite": {
+            filename: "tchaik_lakescene.mid"
+        },
+        "Pyotr Ilyich Tchaikovsky - Danse russe: Trepak": {
+            filename: "tchaik_trepak.mid"
+        },
+        "Antonio Vivaldi - Spring from The Four Seasons, RV 269": {
+            filename: "vivaldi_4s_spring.mid"
+        },
+        "Antonio Vivaldi - Winter from The Four Seasons, RV 297": {
+            filename: "vivaldi_4s_winter.mid"
+        },
+        "Unknown Composer - The Maidens": {
+            filename: "wanton_maidens.mid"
         }
     };
 }
