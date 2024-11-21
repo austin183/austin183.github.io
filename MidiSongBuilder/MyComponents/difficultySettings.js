4f72f41a05506faafdc4dae79d780ef7a7908a3f
Austin <austin183@gmail.com>
2024-11-20 19:36:12 -0600

    Changing the slider bars for difficulty over to predefined settings in order to support cookie based high scores for a user per difficulty.

---
+++ MidiSongBuilder/MyComponents/difficultySettings.js
@@ -0,0 +1,20 @@
+function getDifficultySettings(){
+    return {
+        "A Bit Less Please": {
+            minNoteDistance: .5,
+            minNoteDuration: .2
+        },
+        "Normal": {
+            minNoteDistance: .4,
+            minNoteDuration: .1
+        },
+        "A Bit More Please": {
+            minNoteDistance: .3,
+            minNoteDuration: .1
+        },
+        "Insanity": {
+            minNoteDistance: .05,
+            minNoteDuration: .01
+        }
+    };
+}
