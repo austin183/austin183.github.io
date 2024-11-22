13b3edcf4b0e4768e6f721f66a45892fe3e40748
Austin <austin183@gmail.com>
2024-11-22 10:32:03 -0600

    Adding button to copy high scores to clipboard in debug mode in preparation for adding Developer scores as a score to beat.

--- MidiSongBuilder/MyComponents/highScoreTracker.js
+++ MidiSongBuilder/MyComponents/highScoreTracker.js
@@ -9,12 +9,28 @@ function getHighScoreTracker(){
             return value;            
         },
         setHighScore: function(midiFileName, difficulty, highScore) {
-          debugger;
           var key = midiFileName + "_" + difficulty;
           const currentHighScore = localStorage.getItem(key);
           if(currentHighScore == null || parseInt(currentHighScore) < highScore){
             localStorage.setItem(key, highScore.toString());
           }
+        },
+        exportHighScoresToClipBoardAsJsonString: function(){
+          const highScores = {};
+          for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (key.includes("_")) { // assuming the key format is "midiFileName_difficulty"
+              highScores[key] = localStorage.getItem(key);
+            }
+          }
+          const jsonStr = JSON.stringify(highScores, null, 2); // pretty-print with indentation
+          
+          navigator.clipboard.writeText(jsonStr).then(() => {
+            console.log("High scores copied to clipboard as JSON string");
+          }).catch(err => {
+            console.error("Failed to copy high scores to clipboard: ", err);
+            alert('JSON string was not copied to clipboard.  Please try to copy it by hand here: ```' + jsonStr + '```');
+          });
         }
     };
 }
