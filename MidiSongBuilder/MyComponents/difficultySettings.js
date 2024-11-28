22cfaed597d9336cb5395f7af71d2b7ecb3b6abc
Austin <austin183@gmail.com>
2024-11-27 19:04:51 -0600

    Changing target note per minute to line up with the difficulties added at the song level.  Still need to work on algorithm to approach target notes per minute for play provided midi files.

--- MidiSongBuilder/MyComponents/difficultySettings.js
+++ MidiSongBuilder/MyComponents/difficultySettings.js
@@ -4,25 +4,25 @@ function getDifficultySettings(){
             difficultyKey: "easy",
             minNoteDistance: .5,
             minNoteDuration: .2,
-            targetNotesPerMinute: 60
+            targetNotesPerMinute: 50
         },
         "Normal": {
             difficultyKey: "normal",
             minNoteDistance: .3,
             minNoteDuration: .17,
-            targetNotesPerMinute: 80
+            targetNotesPerMinute: 60
         },
         "A Bit More Please": {
             difficultyKey: "hard",
             minNoteDistance: .2,
             minNoteDuration: .17,
-            targetNotesPerMinute: 100
+            targetNotesPerMinute: 80
         },
         "Insanity": {
             difficultyKey: "crazy",
             minNoteDistance: 0.18,
             minNoteDuration: .17,
-            targetNotesPerMinute: 120
+            targetNotesPerMinute: 100
         }
     };
 }
