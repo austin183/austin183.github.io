148ff86b08851a314905203aa160e27a21696401
Austin <austin183@gmail.com>
2024-12-18 19:15:44 -0600

    Rewriting the difficultySettingsCalculator to run through several combinations of duration and distance to try to approace the target notes per minute set for each difficulty.  Still doesn't replace the need for some hand adjustments per song, but will do okay for user provided midi files.

--- MidiSongBuilder/MyComponents/difficultySettingsCalculator.js
+++ MidiSongBuilder/MyComponents/difficultySettingsCalculator.js
@@ -1,35 +1,5 @@
 function getDifficultySettingsCalculator(){
 var statCalculators = {
-    calculateMedian: function(arr) {
-        const sortedArr = arr.slice().sort((a, b) => a - b);
-        const midIndex = Math.floor(sortedArr.length / 2);
-        return sortedArr[midIndex];
-    },
-    calculateWeightedAverage: function (keys, values) {
-        const totalValue = keys.reduce((acc, key) => acc + values[key], 0);
-        const weightedSum = keys.reduce((acc, key) => acc + values[key] * parseFloat(key), 0);
-        return weightedSum / totalValue;
-    },
-    getQuantile: function(quantile, stats) {
-        var keys = Object.keys(stats);
-        var expandedArray = [];
-      
-        // Expand the object into an array
-        keys.forEach(function(key) {
-          for (var i = 0; i < stats[key]; i++) {
-            expandedArray.push(parseFloat(key));
-          }
-        });
-      
-        // Sort the array in ascending order
-        expandedArray.sort((a, b) => a - b);
-      
-        // Calculate the index of the desired quantile
-        var index = Math.floor(quantile * expandedArray.length);
-      
-        // Return the value at that index
-        return expandedArray[index];
-    },
     gatherNoteDistributions: function(song, invertedKeyNoteMap){
         var noteDistanceStats = {};
         var noteDurationStats = {};
@@ -61,6 +31,18 @@ var statCalculators = {
                 previousNote = currentNote;
             }                
         }
+        const sortedNoteDistanceStats = Object.keys(noteDistanceStats).sort().reduce((acc, key) => {
+            acc[key] = noteDistanceStats[key];
+            return acc;
+        }, {});
+        noteDistanceStats = sortedNoteDistanceStats;
+
+        const sortedNoteDurationStats = Object.keys(noteDurationStats).sort().reduce((acc, key) => {
+            acc[key] = noteDurationStats[key];
+            return acc;
+        }, {});
+        noteDurationStats = sortedNoteDurationStats;
+
         return {
             noteDistanceStats: noteDistanceStats,
             noteDurationStats: noteDurationStats,
@@ -73,71 +55,56 @@ return {
     getSongStats: function(song, invertedKeyNoteMap){
         return statCalculators.gatherNoteDistributions(song, invertedKeyNoteMap);
     },
-    getDifficultiesForSongByQuantiles: function(song, invertedKeyNoteMap){
+    getTargetVisibleField: function(targetNotesPerMinute, song, invertedKeyNoteMap, visibleFieldFilterer, keyRenderInfo, notesCanvas, songNoteRenderer, songEnd){
         const noteDistribution = statCalculators.gatherNoteDistributions(song, invertedKeyNoteMap);
-        const distanceQuantiles = [0.65, 0.50, 0.40, .25];
-        const durationQuantiles = [0.35, 0.30, 0.15, .15];
-        const absoluteMinimumDistance = .17; //This is to keep the game from becoming overwhelming for some songs.
         
-        const distanceQuantileValues = distanceQuantiles.map(q => statCalculators.getQuantile(q, noteDistribution.noteDistanceStats));
-        const durationQuantileValues = durationQuantiles.map(q => statCalculators.getQuantile(q, noteDistribution.noteDurationStats));
-        return {
-          "easy": {
-            difficultyKey: "easy",
-            minNoteDistance: distanceQuantileValues[0] < absoluteMinimumDistance ? absoluteMinimumDistance : distanceQuantileValues[0],
-            minNoteDuration: durationQuantileValues[0]
-          },
-          "normal": {
-            difficultyKey: "normal",
-            minNoteDistance: distanceQuantileValues[1] < absoluteMinimumDistance ? absoluteMinimumDistance : distanceQuantileValues[1],
-            minNoteDuration: durationQuantileValues[1]
-          },
-          "hard": {
-            difficultyKey: "hard",
-            minNoteDistance: distanceQuantileValues[2] < absoluteMinimumDistance ? absoluteMinimumDistance : distanceQuantileValues[2],
-            minNoteDuration: durationQuantileValues[2]
-          },
-          "crazy": {
-            difficultyKey: "crazy",
-            minNoteDistance: distanceQuantileValues[3] < absoluteMinimumDistance ? absoluteMinimumDistance : distanceQuantileValues[3],
-            minNoteDuration: durationQuantileValues[3]
-          }
-        };
-    },
-    getDifficultiesForSongByMedianDistanceAndWeightedAverage: function(song, invertedKeyNoteMap){
-        const noteDistribution = statCalculators.gatherNoteDistributions(song, invertedKeyNoteMap);
-        const absoluteMinimumDistance = .17; //This is to keep the game from becoming overwhelming for some songs.
-
-        // Calculate median note distance
-        const noteDistances = Object.keys(noteDistribution.noteDistanceStats).map(key => parseFloat(key));
-        const medianNoteDistance = statCalculators.calculateMedian(noteDistances);
-
-        // Calculate weighted average note duration
-        const noteDurations = Object.keys(noteDistribution.noteDurationStats);
-        const weightedAverageDuration = statCalculators.calculateWeightedAverage(noteDurations, noteDistribution.noteDurationStats);
-
-        return {
-            "easy": {
-                difficultyKey: "easy",
-                minNoteDistance: (medianNoteDistance * 1.1) < absoluteMinimumDistance ? absoluteMinimumDistance : (medianNoteDistance * 1.1),
-                minNoteDuration: weightedAverageDuration / 3.3
-            },
-            "normal": {
-                difficultyKey: "normal",
-                minNoteDistance: (medianNoteDistance * 1) <  absoluteMinimumDistance ? absoluteMinimumDistance : (medianNoteDistance * 1),
-                minNoteDuration: weightedAverageDuration / 4
-            },
-            "hard": {
-                difficultyKey: "hard",
-                minNoteDistance: (medianNoteDistance * .9) <  absoluteMinimumDistance ? absoluteMinimumDistance : (medianNoteDistance * .9),
-                minNoteDuration: weightedAverageDuration / 4
-            },
-            "crazy": {
-                difficultyKey: "crazy",
-                minNoteDistance: (medianNoteDistance * .7)  <  absoluteMinimumDistance ? absoluteMinimumDistance : (medianNoteDistance * .7),
-                minNoteDuration: weightedAverageDuration / 5
+        // Initialize best result
+        let allCombinationsWithin10NotesPerMinute = [];
+        let bestResult = null;
+    
+        // Iterate over all combinations of distances and durations
+        Object.keys(noteDistribution.noteDistanceStats).sort((a, b) => parseFloat(b) - parseFloat(a)).forEach(distance => {
+            if (parseFloat(distance) <= 0.6) {
+                Object.keys(noteDistribution.noteDurationStats).forEach(duration => {                    
+                    if (parseFloat(duration) <= 0.6) {
+                        // Calculate visible field
+                        const visibleField = visibleFieldFilterer.filterToFullVisibleField(song, parseFloat(distance), parseFloat(duration), invertedKeyNoteMap, keyRenderInfo, notesCanvas, songNoteRenderer);
+                        const currentNotesPerMinute = Math.floor((visibleField.length / songEnd) * 60);
+                        const currentDistanceToTarget = Math.abs(currentNotesPerMinute - targetNotesPerMinute);
+                        if(currentDistanceToTarget < 10){
+                            allCombinationsWithin10NotesPerMinute.push({
+                                notesPerMinute: currentNotesPerMinute,
+                                minNoteDistance: parseFloat(distance),
+                                minNoteDuration: parseFloat(duration),
+                                visibleField: visibleField
+                            });
+                        }
+                        if (bestResult === null || currentDistanceToTarget < Math.abs(bestResult.notesPerMinute - targetNotesPerMinute)) {
+                            bestResult = {
+                                notesPerMinute: currentNotesPerMinute,
+                                minNoteDistance: distance,
+                                minNoteDuration: duration,
+                                visibleField: visibleField
+                            };
+                        }
+                    }
+                });
             }
-        };
+        });
+    
+        //return arrayElement with greatest minNoteDistance
+        if (allCombinationsWithin10NotesPerMinute.length > 0) {
+            debugger;
+            return allCombinationsWithin10NotesPerMinute.reduce((maxMinNoteDistanceObject, currentObject) => {
+                if (currentObject.minNoteDistance > maxMinNoteDistanceObject.minNoteDistance) {
+                    return currentObject;
+                }
+                return maxMinNoteDistanceObject;
+            }, allCombinationsWithin10NotesPerMinute[0]);
+        } else {
+            // handle the case when the array is empty
+            return bestResult; // or any other value that makes sense for your application
+        }
     }
 };
 }
